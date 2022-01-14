bd77c5738bd192cbd8396643381a352b52bd3236
loren <lorenmichaelh@gmail.com>
2022-01-14 16:17:17 -0600

    commit tree 1a19893026b7b31f014cf3d873c1b93bfbe26bb1

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for(let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    // const startChar = word[i];
+    // const endChar = word[j];
+    if (word[i]!== word[j]) return false;
+  } return true
 }
 
 /* 
-  Add your pseudocode here
+  We want to iterate through the word letter by letter, returning false if any two letters do not match until the function reaches the middle of the word
 */
 
 /*
-  Add written explanation of your solution here
+  I am using the index numbers of the characters in the word to formulate a way to look at the first character and the last character, then the second character and second to last character, and so on, until I reach the middle of the word. If any any point, the characters do not match, the function should stop and return a false.
 */
 
 // You can run `node index.js` to view these console logs
